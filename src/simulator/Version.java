6ce7c05807097b3fa3fad270af9473d03f65d06a
Dave <github@aukware.com>
2018-05-03 08:49:04 -0400

    CASIM-20: Bumped the version number and removed the misc folder

--- src/simulator/Version.java
+++ src/simulator/Version.java
@@ -2,7 +2,7 @@ package simulator;
 
 public class Version {
     public static final int major = 1;
-    public static final int minor = 5;
+    public static final int minor = 6;
     public static final int build = 0;
     public static final String version = String.format("%d.%d", major, minor);
 }
